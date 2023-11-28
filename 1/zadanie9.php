0fd035193bfea265327df8c3df04333bda07eb98
glebfarmon <glebfarmon@gmail.com>
2023-11-28 22:47:39 +0100

    meow

---
+++ 1/zadanie9.php
@@ -0,0 +1,10 @@
+<?php
+$liczba = 3;
+
+do {
+    echo "$liczba<br>";
+    $liczba += 3;
+    //w sumie giga ezz, chyba nie potrzebuje nikt tlumaczenia, po prostu
+    //wypisujemy wszistkie liczby dodajc do nich co raz 3 dopoki ona
+    //nie bedzie wieksza od 100
+} while($liczba < 100);
